ee2dfde770000440d552d7673a798021816d0c41
ТогузбаевА <abai2010@bk.ru>
2019-02-02 13:54:47 +0600

    Добавьте файлы проекта.

---
+++ AzureStorage/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount account = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient serviceClient = account.CreateCloudBlobClient();
+
+            // Create container. Name must be lower case.
+            Console.WriteLine("Creating container...");
+            var container = serviceClient.GetContainerReference("mycontainer");
+            container.CreateIfNotExistsAsync().Wait();
+
+            // write a blob to the container
+            CloudBlockBlob blob = container.GetBlockBlobReference("helloworld.txt");
+            blob.UploadTextAsync("Hello, World!").Wait();
+        }
+    }
+}
